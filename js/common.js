4f88a8ceee8a3bc9cc41e1495960c3e816c7f114
rhkanrdl <emrdl7@gmail.com>
2018-11-27 14:42:08 +0900

    끝

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $("body.login").backgroundCycle({
+        imageUrls: [
+            'images/bg_01.jpg',
+            'images/bg_02.jpg',
+            'images/bg_03.jpg',
+            'images/bg_04.jpg'
+        ],
+        fadeSpeed: 2000,
+        duration: 5000,
+        backgroundSize: SCALING_MODE_COVER
+    });
+
+});
+
